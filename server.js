f2849b77b115275f2b8c9e71e159cde4ed6d92bb
MMiszy <lupinek7@gmail.com>
2014-07-22 10:24:18 +0200

    Fix Angular issues

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 connect().use(serveStatic(__dirname + '/public')).listen(process.env.PORT || process.env.VMC_APP_PORT || 3333);
+console.log(process.env.PORT || process.env.VMC_APP_PORT || 3333);
